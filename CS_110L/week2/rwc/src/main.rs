6268e74341ade64ea553abeec608a44dc6acb1a3
qiuanran <2569539312@qq.com>
2024-02-28 20:39:54 +0800

    finish week2

--- CS_110L/week2/rwc/src/main.rs
+++ CS_110L/week2/rwc/src/main.rs
@@ -1,5 +1,7 @@
-use std::env;
+use std::{env, io};
+use std::fs::File;
 use std::process;
+use std::io::BufRead;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,4 +11,18 @@ fn main() {
     }
     let filename = &args[1];
     // Your code here :)
+    let mut chars = 0;
+    let mut lines = -1;
+    let mut words = 0;
+    
+    let file = File::open(filename).expect("FILE CANT BE FOUND");
+
+    for line in io::BufReader::new(file).lines() {
+        let line = line.unwrap();
+        lines += 1;
+        words += line.split_whitespace().count();
+        chars += line.chars().count(); 
+    }
+
+    println!("{} {} {} {}",lines,words,chars,filename);
 }
